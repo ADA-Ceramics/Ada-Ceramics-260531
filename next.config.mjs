684ed7f4f3bc30497b64cf48c2436194e63662ef
ADA-Ceramics <sukichoi85@gmail.com>
2026-06-01 01:50:33 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  swcMinify: true,
+  productionBrowserSourceMaps: false,
   images: {
-    unoptimized: false,
-    formats: ['image/webp'],
+    formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  experimental: {
+    optimizeCss: true,
   },
 }
 
