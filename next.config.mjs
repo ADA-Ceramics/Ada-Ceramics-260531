e0e9e3f98554e6c9036f7e7694c47b77c71f0987
ADA-Ceramics <sukichoi85@gmail.com>
2026-06-07 20:29:45 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // 开启 Gzip/Brotli 压缩（Pingdom 那个 F 级关键项）
-  compress: true,
   images: {
     unoptimized: false,
     formats: ['image/webp'],
@@ -18,7 +16,7 @@ const nextConfig = {
   },
   productionBrowserSourceMaps: false,
 
-  // 官方CSS优化，自动内联关键CSS、消除渲染阻塞，SEO友好
+  // 新增：官方CSS优化，自动内联关键CSS、消除渲染阻塞，SEO友好
   experimental: {
     optimizeCss: true
   }
